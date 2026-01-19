769a9c69d317f3282ec58a2ee3233f679020b6c8
Adithiinchure <adithiinchurai@gmail.com>
2026-01-19 21:42:29 +0530

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -1,11 +1,10 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def get_embedding(text):
-    embedding = model.encode(text)
-    return embedding.tolist()
+    return model.encode(text).tolist()
 
 def cosine_similarity(vec1, vec2):
     v1 = np.array(vec1)
